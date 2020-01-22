6d8bf909dd786a8ffe9ff7500fe37668b7ff00c3
Tim D'hoore <timdhoore@gmail.com>
2020-01-22 19:46:28 +0100

    created new work view

--- src/components/pages/Work.jsx
+++ src/components/pages/Work.jsx
@@ -9,7 +9,11 @@ const Work = props => {
         <h2>Work</h2>
       </header>
       <div className="workHolder">
+        <WorkItem position={-2} />
+        <WorkItem position={-1} />
         <WorkItem />
+        <WorkItem position={1} />
+        <WorkItem position={2} />
         <NavButtons />
       </div>
     </section>
--- src/components/pages/workComponents/WorkItem.jsx
+++ src/components/pages/workComponents/WorkItem.jsx
@@ -4,12 +4,21 @@ import image from "../../../assets/img/tester.png";
 
 const WorkItem = props => {
   return (
-    <Link to={"/work"} className="workItem">
+    <Link
+      to={"/work"}
+      className={
+        props.position
+          ? `workItem workItemBack workItemBack${props.position}`
+          : `workItem`
+      }
+    >
       <article>
-        <header>
-          <h3>Oreo cake</h3>
-          <p>category</p>
-        </header>
+        <div className="headerWrapper">
+          <header>
+            <h3>Oreo cake</h3>
+            <p>category</p>
+          </header>
+        </div>
         <div className="imgHolder">
           <img src={image} alt={props.title} />
         </div>
